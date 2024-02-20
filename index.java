e4ff74d656a3429d179843bd86242d1cb4328588
nikhil9574 <nikhilprajapati2106@gmail.com>
2024-02-20 15:16:37 +0530

    all changes are done

--- index.java
+++ index.java
@@ -1,5 +1,11 @@
+
 class A5{
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("ten time ten");
+        }
+        System.out.println("done");
     }
+
 }
---
+++ test1.java
@@ -0,0 +1,8 @@
+class test1{
+    public static void main(String[] args) {
+        for (int i = 0; i <7 ; i++) {
+            System.out.println("seven time seven");
+        }
+
+    }
+}
